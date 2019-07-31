cfc5e0b4e637177dbc54c5f18ee06e9273e520a6
sodastereo <f_er_@hotmail.com>
2019-07-31 02:24:17 -0400

    Updates

---
+++ precache-manifest.4992853368e53d0b5af22b5f20c841e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5960bc8138fa4d90ebeb49ac6ad90d5",
+    "url": "/sodastereo87/google_search_react.git/index.html"
+  },
+  {
+    "revision": "a06ee76db5bb1a9911b3",
+    "url": "/sodastereo87/google_search_react.git/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/sodastereo87/google_search_react.git/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "a06ee76db5bb1a9911b3",
+    "url": "/sodastereo87/google_search_react.git/static/js/main.7736fe40.chunk.js"
+  },
+  {
+    "revision": "63452bfe1119f9ce470f",
+    "url": "/sodastereo87/google_search_react.git/static/js/runtime~main.691ed2eb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sodastereo87/google_search_react.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/sodastereo87/google_search_react.git/precache-manifest.4992853368e53d0b5af22b5f20c841e2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sodastereo87/google_search_react.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
