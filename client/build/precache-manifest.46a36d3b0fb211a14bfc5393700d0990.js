8f058f1a1383c448980006b453e6378a0e81d30f
sodastereo <f_er_@hotmail.com>
2019-08-03 00:59:07 -0400

    css added

---
+++ client/build/precache-manifest.46a36d3b0fb211a14bfc5393700d0990.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22069cbcd40ad1cf0df1822e150691f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf22c7489bdf264c8605",
+    "url": "/static/css/main.ce185f8f.chunk.css"
+  },
+  {
+    "revision": "24e633a2738d3f1de0a8",
+    "url": "/static/js/2.2ab01603.chunk.js"
+  },
+  {
+    "revision": "cf22c7489bdf264c8605",
+    "url": "/static/js/main.002ed095.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
